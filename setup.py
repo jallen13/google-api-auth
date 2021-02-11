620674ef58b6581c7c09b8b9445699fd4a2b7d71
John Allen <55411821+jallen13@users.noreply.github.com>
2021-02-10 22:38:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(name='google-api-auth',
       version='0.1.0',
-      description = 'Google APIs authentication,
+      description = 'Google APIs authentication',
       author = 'John Allen',
       url = 'https://github.com/jallen13/google-api-auth.git',
       packages = find_packages()
